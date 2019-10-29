f0050c09c29ef75ce1b4893ad0c02588c1fe5347
Nahuel Balsamo <nbalsamo@elementum.com>
2019-10-29 18:43:01 -0300

    Siguiente Clase

--- js/index.js
+++ js/index.js
@@ -17,15 +17,97 @@
 // 7 Finalicemos implementando disminuirSaldo de Cuenta y retirarPlata de Cajero
 //     - La cuenta esta encargada de revisar que la disminucion sea valida, sino no modificar el saldo y avisar al cajero si el retiro fue exitoso o no.
 //     - El Cajero debe hacer console log si el retiro fue exitoso o no y el estado actual de la cuenta luego del retiro (solo si fue exitoso).
+var Cuenta = {
+  nro: 1,
+  usuario: 'asd',
+  clave: 'asd',
+  saldo: 1000,
 
+  incrementarSaldo: function(monto) {
+    this.saldo += monto;
+  },
+
+  disminuirSaldo: function(monto) {
+    if (this.saldo - monto < 0) {
+      return false;
+    } else {
+      this.saldo -= monto;
+      return true;
+    }
+  }
+};
+
+var Autorizador = {
+  validarAcceso: function(cuentas, nroCuenta, usuario, clave) {
+    var cuentaBuscada = null;
+
+    cuentas.forEach(function(cuenta) {
+      if (cuenta.nro === nroCuenta && cuenta.usuario === usuario && cuenta.clave === clave) {
+        cuentaBuscada = cuenta;
+      }
+    });
+
+    return cuentaBuscada;
+  }
+};
+
+var Cajero = {
+  cuentas: [Cuenta],
+  autorizador: Autorizador,
+  
+  depositarPlata: function(nroCuenta, usuario, clave, monto) {
+    var autorizado = this.autorizador.validarAcceso(this.cuentas, nroCuenta, usuario, clave);
+    if (autorizado) {
+      autorizado.incrementarSaldo(monto);
+      console.log('El nuevo saldo de su cuenta es: ' + autorizado.saldo);
+    } else {
+      console.log('No ingresaste un nro de cuenta o clave correcta');
+    }
+  },
+
+  retirarPlata: function(nroCuenta, usuario, clave, monto) {
+    var autorizado = this.autorizador.validarAcceso(this.cuentas, nroCuenta, usuario, clave);
+    if (autorizado) {
+      var exito = autorizado.disminuirSaldo(monto);
+      if (exito) {
+        console.log('Tu retiro fue exitoso. El nuevo saldo de su cuenta es: ' + autorizado.saldo);
+      } else {
+        console.log('No es posible retirar ese monto de tu cuenta.');
+      }
+    } else {
+      console.log('No ingresaste un nro de cuenta o clave correcta');
+    }
+  }
+};
 
 
 // Ejercicios extra:
-// Poner en el html 4 inputs: nroCuenta, usuario, clave, monto
-// Poner 2 botones: retirar, depositar
-// Bindear el click de ambos botones para que interactuen con los metodos del cajero (asi como estan)
+// - Poner en el html 4 inputs: nroCuenta, usuario, clave, monto
+// - Poner 2 botones: retirar, depositar
+// - Bindear el click de ambos botones para que interactuen con los metodos del cajero (asi como estan)
 
 // Proximos desafios:
-// Cuenta con limite al descubierto
+// Cuenta con limite al descubierto:
+// 1 - Armar un nuevo Objeto literal CuentaConDescubierto con las mismas propiedades y metodos que Cuenta
+// 2 - Agregar/Modificar lo necesario para que esta cuenta permita "girar en descubierto" limitando a un valor maximo definido por la cuenta
+// 3 - Agregar esta nueva cuenta al listado de cuentas del Cajero e interactuar con ambas cuentas y ver como funciona
+
 // Autorizador para administradores
-// Cajero para administradores (sin cambiar su logica)
+// 4 - Armar un nuevo Objeto literal AutorizadorAdmin con las mismas propiedades y metodos que Autorizador
+// 5 - Agregar/Modificar lo necesario para que este autorizador permita utilizando un usuario y clave maestra acceda a la cuenta buscada segun el nroCuenta
+// 6 - Cambiar el Autorizador que utiliza el Cajero e interactuar con ambas cuentas y ver como funciona
+
+// Hasta aca vimos que el codigo del Cajero fue 100% reutilizable, sin embargo las cuentas y los autorizadores no. Hagamos que sea mas reutilizable el codigo de las Cuentas.
+// 7 - Crear una funcion llamada crearCuenta que reciba por parametro: nro, usuario, clave y saldo.
+// 8 - Hacer que esta funcion cree un objeto literal de tipo Cuenta, asigne los valores recibidos y agregue la cuenta a la lista de cuentas del Cajero.
+// 9 - Desde la consola, crear un par de cuentas y luego intentar interactuar con ellas.
+
+// Preguntas:
+// - Quien tiene actualmente la responsabilidad de crear cuentas?
+// - Quien deberia tener la responsabilidad de crear cuentas?
+// - Como actualizarian el Diagrama de Clases para incluir estos cambios?
+
+// Ejercicios extra:
+// - Codear estos ultimos cambios en el diagrama
+// - Poner 1 boton mas: crear cuenta
+// - Bindear el click del boton para que cree una nueva cuenta utilizando los valores de los inputs y la agregue al cajero (utilizar lo que ya esta codeado)
